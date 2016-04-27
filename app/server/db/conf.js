228343c132ce1af49202e7ba1bb4ffa6f974ccde
bwinckell <bwinckell@companeo.com>
2016-04-27 10:04:11 +0200

    remove conf old

--- app/server/db/conf.js
+++ app/server/db/conf.js
@@ -5,21 +5,6 @@ const conf = {
         'DB_NAME': 'test',
         'DB_SUPERUSER': 'postgres',
         'DB_SUPERPASSWORD': ''
-    },
-    'branch': {
-        'DB_HOST': 'pg92-ppd.companeo.local',
-        'DB_PORT': '5432',
-        'DB_NAME': 'companeo_v6',
-        'DB_SUPERUSER': 'companeo',
-        'DB_SUPERPASSWORD': 'qdfkdskf'
-    },
-    'recette': {
-        'DB_HOST': 'pg92-ppd.companeo.local',
-        'DB_PORT': '5432',
-        'DB_NAME': 'companeo_v6',
-        'DB_SUPERUSER': 'companeo',
-        'DB_SUPERPASSWORD': 'qdfkdskf'
     }
 }
-// laure.pinault@bodet-software.com
 module.exports = conf;
